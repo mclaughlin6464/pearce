65900668c98ee1286591de70b91ab42a76be21e2
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-09-05 16:29:05 -0700

    Added a setup.py, still figuring it out.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pearce',
+        version='0.1',
+        description='Pratical Emulation for Analysis and Regression of Cosmological Enviornments.',
+        author='Sean McLaughlin',
+        author_email='swmclau2@stanford.edu',
+        url='https://github.com/mclaughlin6464/pearce',
+        packages=['pearce'])
