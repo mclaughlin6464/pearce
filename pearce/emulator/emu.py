47a25f8617f606421d408653ee64b1ec47b0f029
mclaughlin6464 <mclaughlin6464@gmail.com>
2016-09-08 13:30:37 -0700

    Added an emulator test, and gonna do a different test for the training data.

--- pearce/emulator/emu.py
+++ pearce/emulator/emu.py
@@ -15,14 +15,14 @@ from george.kernels import *
 from .trainingData import PARAMS, GLOBAL_FILENAME
 from .ioHelpers import global_file_reader, xi_file_reader
 
-class Emu(object):
 
-    #NOTE I believe that I'll have to subclass for the scale-indepent and original versions.
-    #I'm going to write the original up so I can understand how I want this to work in particular, and may
+class Emu(object):
+    # NOTE I believe that I'll have to subclass for the scale-indepent and original versions.
+    # I'm going to write the original up so I can understand how I want this to work in particular, and may
     # have to copy a lot of it to the subclass
 
-    #TODO load initial guesses from file.
-    def __init__(self, training_dir,params=PARAMS, independent_variable='xi',fixed_params = {}):
+    # TODO load initial guesses from file.
+    def __init__(self, training_dir, params=PARAMS, independent_variable='xi', fixed_params={}):
 
         '''
         # TODO change name of type
@@ -35,20 +35,19 @@ class Emu(object):
         if independent_variable == 'bias':
             raise NotImplementedError("I have to work on how to do xi_mm first.")
 
-        assert independent_variable in {'xi', 'r2xi'} #no bias for now.
+        assert independent_variable in {'xi', 'r2xi'}  # no bias for now.
 
         self.ordered_params = params
 
         self.get_training_data(self, training_dir, independent_variable, fixed_params)
         self.build_emulator(independent_variable, fixed_params)
 
-
     # I can get LHC from the training data. If any coordinate equals any other in its column we know!
-    def get_training_data(self,training_dir, independent_variable, fixed_params):
+    def get_training_data(self, training_dir, independent_variable, fixed_params):
         '''Implemented in subclasses. '''
         pass
 
-    def build_emulator(self,independent_variable, fixed_params):
+    def build_emulator(self, independent_variable, fixed_params):
         '''
         Initialization of the emulator from recovered training data.
         :param independent_variable:
@@ -69,7 +68,7 @@ class Emu(object):
         kernel = a * ExpSquaredKernel(self.metric, ndim=self.ndim)
         self.gp = george.GP(kernel)
         # gp = george.GP(kernel, solver=george.HODLRSolver, nleaf=x.shape[0]+1,tol=1e-18)
-        self.fixed_params = fixed_params #remember which params were fixed
+        self.fixed_params = fixed_params  # remember which params were fixed
         self.gp.compute(self.x, self.yerr)  # NOTE I'm using a modified version of george!
 
     # Not sure this will work at all in an LHC scheme.
@@ -138,7 +137,7 @@ class Emu(object):
                 # Approximately true, may need to revisit
                 # yerr[idx * NBINS:(idx + 1) * NBINS] = np.sqrt(np.diag(cov)) / (xi * np.log(10))
                 y_err[idx] = np.sqrt(np.diag(cov)) / (
-                xi * np.log(10))  # I think this is right, extrapolating from the above.
+                    xi * np.log(10))  # I think this is right, extrapolating from the above.
             else:  # r2xi
                 y[idx] = xi * rpoints * rpoints
                 y_err[idx] = np.sqrt(np.diag(cov)) * rpoints  # I think this is right, extrapolating from the above.
@@ -161,14 +160,14 @@ class Emu(object):
         :return: initial_guesses, a dictionary of the guess for each parameter
         '''
         if independent_variable == 'xi':
-            ig = {'amp': 0.481, 'logMmin':0.1349,'sigma_logM':0.089,
-                   'logM0': 2.0, 'logM1':0.204, 'alpha':0.039,
-                   'f_c':0.041, 'r':0.040}
-        else:# independent_variable == 'r2xi':
-            ig = {'amp':1}
-            ig.update({p.name:0.1 for p in self.ordered_params})
+            ig = {'amp': 0.481, 'logMmin': 0.1349, 'sigma_logM': 0.089,
+                  'logM0': 2.0, 'logM1': 0.204, 'alpha': 0.039,
+                  'f_c': 0.041, 'r': 0.040}
+        else:  # independent_variable == 'r2xi':
+            ig = {'amp': 1}
+            ig.update({p.name: 0.1 for p in self.ordered_params})
 
-        #remove entries for variables that are being held fixed.
+        # remove entries for variables that are being held fixed.
         for key in fixed_params.iterkeys():
             del ig[key]
 
@@ -183,7 +182,7 @@ class Emu(object):
     def emulate_wrt_r(self, em_params, rpoints):
         pass
 
-    def goodness_of_fit(self, truth_dir, N=None, statistic = 'r2'):
+    def goodness_of_fit(self, truth_dir, N=None, statistic='r2'):
         '''
         Calculate the goodness of fit of an emulator as compared to some validation data.
         :param truth_dir:
@@ -209,14 +208,14 @@ class Emu(object):
 
         values = []
         rbins, _, _ = global_file_reader(path.join(truth_dir, GLOBAL_FILENAME))
-        r_centers = (rbins[:1]+rbins[:-1])/2
+        r_centers = (rbins[:1] + rbins[:-1]) / 2
         for idx in idxs:
             params, true_xi, _ = xi_file_reader(corr_files[idx], cov_files[idx])
             pred_log_xi, _ = self.emulate_wrt_r(params, np.log10(r_centers))
 
             if statistic == 'rmsfd':
                 values.append(np.sqrt(np.mean(((pred_log_xi - np.log10(true_xi)) ** 2) / (np.log10(true_xi) ** 2))))
-            else: #r2
+            else:  # r2
                 SSR = np.sum((pred_log_xi - np.log10(true_xi)) ** 2)
                 SST = np.sum((np.log10(true_xi) - np.log10(true_xi).mean()) ** 2)
 
@@ -226,8 +225,7 @@ class Emu(object):
 
 
 class OriginalRecipe(Emu):
-
-    def get_training_data(self,training_dir, independent_variable, fixed_params):
+    def get_training_data(self, training_dir, independent_variable, fixed_params):
         '''
         Read the training data for the emulator and attach it to the object.
         :param training_dir:
@@ -241,23 +239,23 @@ class OriginalRecipe(Emu):
 
         rbins, cosmo_params, method = global_file_reader(path.join(training_dir, GLOBAL_FILENAME))
 
-        if not fixed_params and method=='LHC':
+        if not fixed_params and method == 'LHC':
             raise ValueError('Fixed parameters is not empty, but the data in training_dir is form a Latin Hypercube. \
                                 Cannot performs slices on a LHC.')
 
         corr_files = sorted(glob(path.join(training_dir, 'xi*.npy')))
         cov_files = sorted(
             glob(path.join(training_dir, '*cov*.npy')))  # since they're sorted, they'll be paired up by params.
-        self.rpoints = (rbins[:-1]+rbins[1:])/2
+        self.rpoints = (rbins[:-1] + rbins[1:]) / 2
         nbins = self.rpoints.shape[0]
-        #HERE
-        npoints = len(corr_files) * nbins # each file contains NBINS points in r, and each file is a 6-d point
+        # HERE
+        npoints = len(corr_files) * nbins  # each file contains NBINS points in r, and each file is a 6-d point
 
-        #HERE
+        # HERE
         varied_params = set([p.name for p in self.ordered_params]) - set(fixed_params.keys())
-        ndim = len(varied_params)   # lest we forget r
+        ndim = len(varied_params)  # lest we forget r
 
-        #not sure about this.
+        # not sure about this.
 
         x = np.zeros((npoints, ndim))
         y = np.zeros((npoints,))
@@ -296,9 +294,8 @@ class OriginalRecipe(Emu):
                 else:
                     file_params.append(np.ones((nbins,)) * params[p.name])
 
-
             x[idx * nbins:(idx + 1) * nbins, :] = np.stack(file_params).T
-            #TODO helper function that handles this part
+            # TODO helper function that handles this part
             # TODO the time has come to do something smarter for bias... I will ignore for now.
             '''
             if independent_variable == 'bias':
@@ -309,8 +306,8 @@ class OriginalRecipe(Emu):
                 y[idx * nbins:(idx + 1) * nbins] = np.log10(xi)
                 # Approximately true, may need to revisit
                 yerr[idx * nbins:(idx + 1) * nbins] = np.sqrt(np.diag(cov)) / (xi * np.log(10))
-                #ycovs.append(cov / (np.outer(xi, xi) * np.log(10) ** 2))  # I think this is right, extrapolating from the above.
-            else: #r2xi
+                # ycovs.append(cov / (np.outer(xi, xi) * np.log(10) ** 2))  # I think this is right, extrapolating from the above.
+            else:  # r2xi
                 y[idx * nbins:(idx + 1) * nbins] = xi * self.rpoints * self.rpoints
                 yerr[idx * nbins:(idx + 1) * nbins] = cov * np.outer(self.rpoints, self.rpoints)
 
@@ -321,14 +318,14 @@ class OriginalRecipe(Emu):
         # NOTE: HACK
         # a reshape may be faster.
         zeros_slice = np.all(x != 0.0, axis=1)
-        #set the results of these calculations.
+        # set the results of these calculations.
         self.ndim = ndim
-        self.x =  x[zeros_slice]
+        self.x = x[zeros_slice]
         self.y = y[zeros_slice]
         self.yerr = yerr[zeros_slice]
 
         self.y_hat = self.y.mean()
-        self.y-=self.y_hat #mean-subtract.
+        self.y -= self.y_hat  # mean-subtract.
 
     def train(self, **kwargs):
         '''
@@ -377,9 +374,9 @@ class OriginalRecipe(Emu):
         input_params.update(em_params)
         assert len(input_params) == self.ndim  # check dimenstionality
         for i in input_params:
-            assert any(i==p.name for p in self.ordered_params)
+            assert any(i == p.name for p in self.ordered_params)
 
-        #i'd like to remove 'r'. possibly requiring a passed in param?
+        # i'd like to remove 'r'. possibly requiring a passed in param?
         t_list = [input_params[p.name] for p in self.ordered_params]
         t_grid = np.meshgrid(*t_list)
         t = np.stack(t_grid).T
@@ -388,10 +385,10 @@ class OriginalRecipe(Emu):
         mu.reshape((-1, len(self.rpoints)))
         errs = np.sqrt(np.diag(cov))
         errs.reshape((-1, len(self.rpoints)))
-        #Note ordering is unclear if em_params has more than 1 value.
+        # Note ordering is unclear if em_params has more than 1 value.
         outputs = []
         for m, e in izip(mu, errs):
-            outputs.append((m,e))
+            outputs.append((m, e))
         return outputs
 
     def emulate_wrt_r(self, em_params, rpoints):
@@ -406,13 +403,12 @@ class OriginalRecipe(Emu):
         :return:
         '''
         vep = dict(em_params)
-        vep.update({'r':rpoints})
+        vep.update({'r': rpoints})
 
         return self.emulate(vep)
 
+
 class ExtraCrispy(Emu):
-
-
     def get_training_data(self, training_dir, independent_variable, fixed_params):
         '''
         Read the training data for the emulator and attach it to the object.
@@ -446,8 +442,8 @@ class ExtraCrispy(Emu):
         # not sure about this.
 
         x = np.zeros((npoints, ndim))
-        y = np.zeros((npoints,nbins))
-        yerr = np.zeros((npoints,nbins))
+        y = np.zeros((npoints, nbins))
+        yerr = np.zeros((npoints, nbins))
 
         warned = False
         num_skipped = 0
@@ -491,11 +487,11 @@ class ExtraCrispy(Emu):
             if independent_variable == 'xi':
                 y[idx, :] = np.log10(xi)
                 # Approximately true, may need to revisit
-                yerr[idx,:] = np.sqrt(np.diag(cov)) / (xi * np.log(10))
+                yerr[idx, :] = np.sqrt(np.diag(cov)) / (xi * np.log(10))
                 # ycovs.append(cov / (np.outer(xi, xi) * np.log(10) ** 2))  # I think this is right, extrapolating from the above.
             else:  # r2xi
-                y[idx,:] = xi * self.rpoints * self.rpoints
-                yerr[idx,:] = cov * np.outer(self.rpoints, self.rpoints)
+                y[idx, :] = xi * self.rpoints * self.rpoints
+                yerr[idx, :] = cov * np.outer(self.rpoints, self.rpoints)
 
         # ycov = block_diag(*ycovs)
         # ycov = np.sqrt(np.diag(ycov))
@@ -526,7 +522,7 @@ class ExtraCrispy(Emu):
             # Update the kernel parameters and compute the likelihood.
             # params are log(a) and log(m)
             self.gp.kernel[:] = p
-            #check this has the right direction
+            # check this has the right direction
             ll = np.sum(self.gp.lnlikelihood(y, quiet=True) for y in self.y)
 
             # The scipy optimizer doesn't play well with infinities.
@@ -536,7 +532,7 @@ class ExtraCrispy(Emu):
         def grad_nll(p):
             # Update the kernel parameters and compute the likelihood.
             self.gp.kernel[:] = p
-            #mean or sum?
+            # mean or sum?
             return -np.mean(self.gp.grad_lnlikelihood(y, quiet=True) for y in self.y)
 
         p0 = self.gp.kernel.vector
@@ -572,12 +568,12 @@ class ExtraCrispy(Emu):
         output = []
         for i, y in enumerate(self.y):
             mu, cov = self.gp.predict(self.y, t)
-            output.append((mu+self.y_hat, np.sqrt(np.diag(cov))))
+            output.append((mu + self.y_hat, np.sqrt(np.diag(cov))))
         # note may want to do a reshape here., Esp to be consistent with the other
         # implementation
         return output
 
-    def emulate_wrt_r(self, em_params, rpoints, kind = 'slinear'):
+    def emulate_wrt_r(self, em_params, rpoints, kind='slinear'):
         '''
         Conveniance function. Add's 'r' to the emulation automatically, as this is the
         most common use case.
@@ -590,12 +586,12 @@ class ExtraCrispy(Emu):
             Kind of interpolation to do, is necessary. Default is slinear.
         :return:
         '''
-        #turns out this how it already works!
+        # turns out this how it already works!
         output = self.emulate(em_params)
-        #don't need to interpolate!
+        # don't need to interpolate!
         if rpoints == self.rpoints:
             return output
-        #TODO check rpoints in bounds!
+        # TODO check rpoints in bounds!
         new_output = []
         for mean, err in output:
             xi_interpolator = interp1d(rpoints, mean, kind=kind)
@@ -604,5 +600,4 @@ class ExtraCrispy(Emu):
             err_interp = interp1d(rpoints, err, kind=kind)
             interp_err = err_interp(rpoints)
             new_output.append((interp_mean, interp_err))
-        return  new_output
-
+        return new_output
