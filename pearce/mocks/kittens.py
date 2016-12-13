527f3a8acb94f038d08941adf3b87becdac6ab47
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-12-12 16:40:01 -0800

    Added a column to include formation time.

--- pearce/mocks/kittens.py
+++ pearce/mocks/kittens.py
@@ -16,7 +16,7 @@ __all__ = ['Bolshoi', 'Multidark', 'Emu', 'Fox', 'MDHR', 'Chinchilla', 'Aardvark
 HLIST_COLS = {'halo_id': (1, 'i8'), 'halo_upid': (6, 'i8'),
               'halo_x': (17, 'f4'), 'halo_y': (18, 'f4'), 'halo_z': (19, 'f4'),
               'halo_vx': (20, 'f4'), 'halo_vy': (21, 'f4'), 'halo_vz': (22, 'f4'),
-              'halo_mvir': (10, 'f4'), 'halo_rvir': (11, 'f4'), 'halo_rs': (12, 'f4')}
+              'halo_mvir': (10, 'f4'), 'halo_rvir': (11, 'f4'), 'halo_rs': (12, 'f4')              'halo_snapnum': (31, 'i8')}
 
 OUTLIST_COLS = {'halo_id': (0, 'i8'), 'halo_upid': (36, 'i8'),
                 'halo_x': (8, 'f4'), 'halo_y': (9, 'f4'), 'halo_z': (10, 'f4'),
