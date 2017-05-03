d3169e20b288400f32073efa9aaf8e849fd8b483
mclaughlin6464 <mclaughlin6464@gmail.com>
2017-05-03 15:30:25 -0700

    Added the priors, going to submit now

--- bin/test_training.py
+++ bin/test_training.py
@@ -3,6 +3,11 @@ from pearce.emulator import make_training_data, parameter, DEFAULT_PARAMS
 
 or_params = DEFAULT_PARAMS
 
+or_params.extend([parameter('mean_occupation_centrals_assembias_param1', -1.0, 1.0),
+                  parameter('disp_func_slope_centrals', 1e-3, 1e3),
+                  parameter('mean_occupation_satellites_assembias_param1', -1.0, 1.0),
+                  parameter('disp_func_slope_satellites', 1e-3, 1e3) ])
+
 make_training_data('/u/ki/swmclau2/Git/pearce/bin/training_config.cfg', or_params)
 
 
