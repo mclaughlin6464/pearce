b3eb27c114bbaa3d152326d2593108fff5f0a380
Sean McLaughlin <mclaughlin6464@gmail.com>
2017-05-21 15:54:36 -0700

    Fixing some bugs in kdtree.

--- pearce/emulator/emu.py
+++ pearce/emulator/emu.py
@@ -1126,13 +1126,15 @@ class ExtraCrispy(Emu):
             #whiten so all distances are the same
             normed_x = (x - x.min(axis=0)) / x.max(axis=0)
             normed_x[np.isnan(normed_x)] = 0.0
-            kdtree = KDTree(normed_x,leafsize = 50)# leafsize = points_per_expert/self.overlap)
+            kdtree = KDTree(normed_x, leafsize = points_per_expert/self.overlap)
             leaves = get_leaves(kdtree)
 
             prev_idx, curr_idx = 0,0
             # If points cannot be evenly divided, there'll be some skipped ones.
             # We'll add them in at the end.
             n_missed = np.sum([(self.overlap*len(leaf)%self.experts)/self.overlap for leaf in leaves])
+            print n_missed, self.x.shape
+
             missed_points = np.zeros(n_missed, dtype=int)
             missed_idx = 0
 
@@ -1163,12 +1165,18 @@ class ExtraCrispy(Emu):
             if n_missed>0:
                 missed_ppe = int(1.0*n_missed*self.overlap/self.experts)
 
+                print missed_points
+                print missed_points.shape, missed_ppe
+
+                curr_idx = prev_idx +missed_ppe
+                print self.x.shape,curr_idx, prev_idx
+
                 for i in xrange(self.experts):
-                    self.x[i, prev_idx:, :] = \
+                    self.x[i, prev_idx:curr_idx, :] = \
                         np.roll(x[missed_points, :], i * missed_ppe / self.overlap, 0)[:missed_ppe, :]
-                    self.y[j, prev_idx:] = \
+                    self.y[j, prev_idx:curr_idx] = \
                         np.roll(y[missed_points], i * missed_ppe / self.overlap, 0)[:missed_ppe]
-                    self.yerr[j, prev_idx:] \
+                    self.yerr[j, prev_idx:curr_idx] \
                         = np.roll(yerr[missed_points], i * missed_ppe / self.overlap, 0)[:missed_ppe]
 
         ndim = x.shape[1]
