Have to fixed some covariance matrix stuff now.

pearce/emulator/emu.py
@@ -99,7 +99,7 @@ class Emu(object):
         # we store redshifts, not scale factors
         self.redshift_bin_centers = 1 / sub_dirs_as - 1
 
-        all_x, all_y, all_ycov = [], [], []
+        all_x, all_y, all_yerr, all_ycov = [], [], [], []
 
         for sub_dir, z in izip(sub_dirs, self.redshift_bin_centers):
 
@@ -142,7 +142,8 @@ class Emu(object):
             ndim = len(self._ordered_params)
             x = np.zeros((npoints, ndim))
             y = np.zeros((npoints,))
-            ycov = np.zeros((npoints, npoints))
+            yerr = np.zeros((npoints,))
+            ycov = np.zeros((scale_nbins, scale_nbins))
 
             warned = False
             num_skipped = 0
@@ -178,19 +179,20 @@ class Emu(object):
                 x[idx * scale_nbins:(idx + 1) * scale_nbins, :] = np.stack(file_params).T
 
                 y[idx * scale_nbins:(idx + 1) * scale_nbins], _cov = self._iv_transform(independent_variable, obs, cov)
-
-                ycov[idx * scale_nbins:(idx + 1) * scale_nbins, idx * scale_nbins:(idx + 1) * scale_nbins] = _cov
+                yerr[idx * scale_nbins:(idx + 1) * scale_nbins] = np.sqrt(np.diag(_cov)
+                ycov += _cov
 
             # remove rows that were skipped due to the fixed thing
             # NOTE: HACK
             # a reshape may be faster.
             # TODO could I use Nonzero, or, better, keep track of the index I need??
             # Yeah isn't this just num_used?
-            #zeros_slice = np.any(x != 0.0, axis=1)
+            zeros_slice = np.any(x != 0.0, axis=1)
 
-            all_x.append(x[:num_used])
-            all_y.append(y[:num_used])
-            all_ycov.append(ycov[:num_used, :num_used])
+            all_x.append(x[zeros_slice])
+            all_y.append(y[zeros_slice])
+            all_yerr.append(yerr[zeros_slice])
+            all_ycov.append(ycov/num_used) #add the average
 
         self.scale_bin_centers = scale_bin_centers
 
@@ -201,7 +203,7 @@ class Emu(object):
             self._ordered_params['z'] = (np.min(self.redshift_bin_centers), np.max(self.redshift_bin_centers))
 
         # TODO sort?
-        return np.vstack(all_x), np.hstack(all_y), block_diag(*all_ycov)
+        return np.vstack(all_x), np.hstack(all_y), np.hstack(all_yerr), block_diag(*all_ycov)
 
     def get_plot_data(self, em_params, training_dir, independent_variable=None, fixed_params={},
                       dependent_variable='r'):