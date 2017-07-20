b2bb4afc4ec7d71032bb6cf0a6c3e337dc6dcfc4
mclaughlin6464 <mclaughlin6464@gmail.com>
2017-07-20 14:43:32 -0700

    Cleaned up some sloppiness from before. Now, I'm going to add an anlytic number density constraint

--- pearce/inference/__init__.py
+++ pearce/inference/__init__.py
@@ -1,2 +1,1 @@
-from .defaultProb import *
 from .runMCMC import *
--- pearce/inference/runMCMC.py
+++ pearce/inference/runMCMC.py
@@ -11,23 +11,43 @@ import numpy as np
 import emcee as mc
 from scipy.linalg import inv
 
-#from .defaultProb import lnprob
-#emu = None
+#liklihood functions need to be defined here because the emulator will be made global
 
 def lnprior(theta, param_names, *args):
-    # TODO copy docs over
-    #global emu
+    """
+    Prior for an MCMC. Default is to assume flat prior for all parameters defined by the boundaries the
+    emulator is built from. Retuns negative infinity if outside bounds or NaN
+    :param theta:
+        The parameters proposed by the sampler.
+    :param param_names
+        The names identifying the values in theta, needed to extract their boundaries
+    :return:
+        Either 0 or -np.inf, depending if the params are allowed or not.
+    """
     for p, t in izip(param_names, theta):
-        low, high = emu.get_param_bounds(p)
+        low, high = _emu.get_param_bounds(p)
         if np.isnan(t) or t < low or t > high:
             return -np.inf
     return 0
 
 
 def lnlike(theta, param_names, r_bin_centers, y, combined_inv_cov):
-    # TODO copy docs over
-    #global emu
-    y_bar = emu.emulate_wrt_r(dict(izip(param_names, theta)), r_bin_centers)[0] 
+    """
+    :param theta:
+        Proposed parameters.
+    :param param_names:
+        The names of the parameters in theta
+    :param r_bin_centers:
+        The centers of the r bins y is measured in, angular or radial.
+    :param y:
+        The measured value of the observable to compare to the emulator.
+    :param combined_inv_cov:
+        The inverse covariance matrix. Explicitly, the inverse of the sum of the mesurement covaraince matrix
+        and the matrix from the emulator. Both are independent of emulator parameters, so can be precomputed.
+    :return:
+        The log liklihood of theta given the measurements and the emulator.
+    """
+    y_bar = _emu.emulate_wrt_r(dict(izip(param_names, theta)), r_bin_centers)[0]
     # should chi2 be calculated in log or linear?
     # answer: the user is responsible for taking the log before it comes here.
     delta = y_bar - y
@@ -37,89 +57,89 @@ def lnlike(theta, param_names, r_bin_centers, y, combined_inv_cov):
 
 
 def lnprob(theta, *args):
-
+    """
+    The total liklihood for an MCMC. Mostly a generic wrapper for the below functions.
+    :param theta:
+        Parameters for the proposal
+    :param args:
+        Arguments to pass into the liklihood
+    :return:
+        Log Liklihood of theta, a float.
+    """
     lp = lnprior(theta, *args)
     if not np.isfinite(lp):
         return -np.inf
 
     return lp + lnlike(theta, *args)
 
-def run_mcmc(_emu,param_names, y, cov, r_bin_centers, fixed_params = {},  nwalkers=1000, nsteps=100, nburn=20, n_cores='all'):
+def run_mcmc(emu, param_names, y, cov, r_bin_centers, fixed_params = {},  nwalkers=1000, nsteps=100, nburn=20, ncores='all'):
+    """
+    Run an MCMC using emcee and the emu. Includes some sanity checks and does some precomputation.
+    Also optimized to be more efficient than using emcee naively with the emulator.
 
-    emu = _emu
-    global emu
+    :param emu:
+        A trained instance of the Emu object
+    :param param_names:
+        Names of the parameters to constrain
+    :param y:
+        data to constrain against
+    :param cov:
+        measured covariance of y
+    :param r_bin_centers:
+        The scale bins corresponding to y
+    :param fixed_params:
+        Any values held fixed during the emulation, default is {}
+    :param nwalkers:
+        Number of walkers for the mcmc. default is 1000
+    :param nsteps:
+        Number of steps for the mcmc. Default is 1--
+    :param nburn:
+        Number of burn in steps, default is 20
+    :param ncores:
+        Number of cores. Default is 'all', which will use all cores available
+    :return:
+        chain, collaposed to the shape ((nsteps-nburn)*nwalkers, len(param_names))
+    """
+    #make emu global so it can be accessed by the liklihood functions
+    _emu = emu
+    global _emu
 
-    assert n_cores == 'all' or n_cores > 0
-    if type(n_cores) is not str:
-        assert int(n_cores) == n_cores
+    assert ncores == 'all' or ncores > 0
+    if type(ncores) is not str:
+        assert int(ncores) == ncores
 
     max_cores = cpu_count()
-    if n_cores == 'all':
-        n_cores = max_cores
-    elif n_cores > max_cores:
-        warnings.warn('n_cores invalid. Changing from %d to maximum %d.' % (n_cores, max_cores))
-        n_cores = max_cores
+    if ncores == 'all':
+        ncores = max_cores
+    elif ncores > max_cores:
+        warnings.warn('ncores invalid. Changing from %d to maximum %d.' % (ncores, max_cores))
+        ncores = max_cores
         # else, we're good!
 
     assert y.shape[0] == cov.shape[0] and cov.shape[1] == cov.shape[0]
     assert y.shape[0] == r_bin_centers.shape[0]
 
     # check we've defined all necessary params
-    assert emu.emulator_ndim == len(fixed_params) + len(param_names) +1 #for r
+    assert _emu.emulator_ndim == len(fixed_params) + len(param_names) +1 #for r
     tmp = param_names[:]
     tmp.extend(fixed_params.keys())
-    assert emu.check_param_names(tmp, ignore = ['r'])
+    assert _emu.check_param_names(tmp, ignore = ['r'])
 
     num_params = len(param_names)
 
-    combined_inv_cov = inv(emu.ycov + cov)
+    combined_inv_cov = inv(_emu.ycov + cov)
 
-    ### speed up parallel emcee by removing some of the objects the emus don't need to make predictons.
-    ### return at the end!
-    '''
-    if n_cores > 1:
-        x_copy, yerr_copy = emu.x, emu.yerr
-        del emu.x
-        del emu.yerr
-
-        try:
-            emulator = emu._emulator
-            emulator_x_copy, emulator_yerr_copy = emulator._x, emulator._yerr
-            del emulator._x
-            del emulator._yerr
-        except AttributeError: #has _emulators instead i.e. ExtraCrispy
-            emulators =  emu._emulators
-            emulator_x_copies, emulator_yerr_copies = [], []
-            for e in emulators:
-                emulator_x_copies.append(e._x)
-                del e._x
-                emulator_yerr_copies.append(e._yerr)
-                del e._yerr
-    '''
     sampler = mc.EnsembleSampler(nwalkers, num_params, lnprob,
-                                 threads=n_cores, args=(param_names, r_bin_centers, y, combined_inv_cov))
+                                 threads=ncores, args=(param_names, r_bin_centers, y, combined_inv_cov))
 
     pos0 = np.zeros((nwalkers, num_params))
     for idx, pname in enumerate(param_names):
-        low, high = emu.get_param_bounds(pname)
+        low, high = _emu.get_param_bounds(pname)
         pos0[:,idx] = np.random.randn(nwalkers)*(np.abs(high-low)/6.0) + (low+high)/2.0
 
     sampler.run_mcmc(pos0, nsteps)
 
-    '''
-    if n_cores > 1:
-        emu.x = x_copy
-        emu.yerr = yerr_copy
 
-        if hasattr(emu, "_emulator"):
-            emu._emulator._x = emulator_x_copy
-            emu._emulator._yerr = emulator_yerr_copy
-        else:
-            emulators =  emu._emulators
-            for e, exc, eyc in zip(emulators, emulator_x_copies, emulator_yerr_copies):
-                e._x  = exc
-                e._yerr = eyc
-    '''
     chain = sampler.chain[:, nburn:, :].reshape((-1, num_params))
 
     return chain
