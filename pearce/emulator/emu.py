954e10befe9ff201e6fb086432b404cc891d58be
Sean McLaughlin <mclaughlin6464@gmail.com>
2017-09-01 14:18:44 -0700

    Merging.

--- pearce/emulator/emu.py
+++ pearce/emulator/emu.py
@@ -115,7 +115,7 @@ class Emu(object):
                 # could maybe do this with sets
                 # Problem is r and z are both acceptable in an LHC, no others.
                 valid_LHC_fp = fp_keys.issubset(set(['r','z']))
-                if sampling_method == 'LHC' and valid_LHC_fp:  # not allowed
+                if sampling_method == 'LHC' and  not valid_LHC_fp:  # not allowed
                     raise ValueError('Fixed parameters is not empty, but the data in data_dir is form a Latin Hypercube. \
                                     Cannot performs slices on a LHC.')
                 else:  # FHC, load up the training file locations
