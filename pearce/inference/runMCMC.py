4bc4f72907e2865f77059e2bee8daf1a11174d79
mclaughlin6464 <mclaughlin6464@gmail.com>
2017-09-25 12:16:49 -0700

    Wrote a generator version of the sampler that I haven't yet tested.

--- pearce/inference/runMCMC.py
+++ pearce/inference/runMCMC.py
@@ -81,6 +81,84 @@ def lnprob(theta, *args):
 
     return lp + lnlike(theta, *args)
 
+def _run_tests(y, cov, r_bin_centers, param_names, fixed_params, ncores, nd_func_name):
+    """
+    Run tests to ensure inputs are valid. Params are the same as in run_mcmc.
+
+    :params:
+        Same as in run_mcmc. See docstring for details.
+
+    :return: ncores, which may be updated if it is an invalid value.
+    """
+
+    assert ncores == 'all' or ncores > 0
+    if type(ncores) is not str:
+        assert int(ncores) == ncores
+
+    max_cores = cpu_count()
+    if ncores == 'all':
+        ncores = max_cores
+    elif ncores > max_cores:
+        warnings.warn('ncores invalid. Changing from %d to maximum %d.' % (ncores, max_cores))
+        ncores = max_cores
+        # else, we're good!
+
+    assert y.shape[0] == cov.shape[0] and cov.shape[1] == cov.shape[0]
+    assert y.shape[0] == r_bin_centers.shape[0]
+
+    # check we've defined all necessary params
+    assert _emu.emulator_ndim == len(fixed_params) + len(param_names) + 1  # for r
+    tmp = param_names[:]
+    assert not any([key in param_names for key in fixed_params])  # param names can't include the
+    tmp.extend(fixed_params.keys())
+    assert _emu.check_param_names(tmp, ignore=['r'])
+
+    assert hasattr(_cat, nd_func_name)
+
+    return ncores
+
+def _resume_from_previous(resume_from_previous, nwalkers, num_params):
+    """
+    Create initial guess by loading previous chain's last position.
+    :param resume_from_previous:
+        String giving the file name of the previous chain to use.
+    :param nwalkers:
+        Number of walkers to initiate. Must be the same as in resume_from_previous
+    :param num_params:
+        Number of params to initiate, must be the same as in resume_from_previous
+
+    :return: pos0, the initial position for each walker in the chain.
+    """
+    # load a previous chain
+    # TODO add error messages here
+    old_chain = np.loadtxt(resume_from_previous)
+    if len(old_chain.shape) == 2:
+        c = old_chain.reshape((nwalkers, -1, num_params))
+        pos0 = c[:, -1, :]
+    else:  # 3
+        pos0 = old_chain[:, -1, :]
+
+    return pos0
+
+def _random_initial_guess(param_names, nwalkers, num_params):
+    """
+    Create a random initial guess for the sampler. Creates a 3-sigma gaussian ball around the center of the prior space.
+    :param param_names:
+        The names of the parameters in the emulator
+    :param nwalkers:
+        Number of walkers to initiate. Must be the same as in resume_from_previous
+    :param num_params:
+        Number of params to initiate, must be the same as in resume_from_previous
+    :return: pos0, the initial position of each walker for the chain.
+    """
+
+    pos0 = np.zeros((nwalkers, num_params))
+    for idx, pname in enumerate(param_names):
+        low, high = _emu.get_param_bounds(pname)
+        pos0[:, idx] = np.random.randn(nwalkers) * (np.abs(high - low) / 6.0) + (low + high) / 2.0
+        # TODO variable with of the initial guess
+
+    return pos0
 
 def run_mcmc(emu, cat, param_names, y, cov, r_bin_centers, obs_nd, obs_nd_err, nd_func_name, \
              fixed_params={}, resume_from_previous=None, nwalkers=1000, nsteps=100, nburn=20, ncores='all'):
@@ -123,30 +201,7 @@ def run_mcmc(emu, cat, param_names, y, cov, r_bin_centers, obs_nd, obs_nd_err, n
     global _emu
     global _cat
 
-    assert ncores == 'all' or ncores > 0
-    if type(ncores) is not str:
-        assert int(ncores) == ncores
-
-    max_cores = cpu_count()
-    if ncores == 'all':
-        ncores = max_cores
-    elif ncores > max_cores:
-        warnings.warn('ncores invalid. Changing from %d to maximum %d.' % (ncores, max_cores))
-        ncores = max_cores
-        # else, we're good!
-
-    assert y.shape[0] == cov.shape[0] and cov.shape[1] == cov.shape[0]
-    assert y.shape[0] == r_bin_centers.shape[0]
-
-    # check we've defined all necessary params
-    assert _emu.emulator_ndim == len(fixed_params) + len(param_names) + 1  # for r
-    tmp = param_names[:]
-    assert not any([key in param_names for key in fixed_params])  # param names can't include the
-    tmp.extend(fixed_params.keys())
-    assert _emu.check_param_names(tmp, ignore=['r'])
-
-    assert hasattr(_cat, nd_func_name)
-
+    ncores= _run_tests(y, cov, r_bin_centers,param_names, fixed_params, ncores, nd_func_name)
     num_params = len(param_names)
 
     combined_inv_cov = inv(np.diag(_emu.ycov) + cov)
@@ -156,20 +211,14 @@ def run_mcmc(emu, cat, param_names, y, cov, r_bin_centers, obs_nd, obs_nd_err, n
                                                        obs_nd, obs_nd_err, nd_func_name))
 
     if resume_from_previous is not None:
+        try:
+            assert nburn == 0
+        except AssertionError:
+            raise AssertionError("Cannot resume from previous chain with nburn != 0. Please change! ")
         # load a previous chain
-        # TODO add error messages here
-        assert nburn == 0
-        old_chain = np.loadtxt(resume_from_previous)
-        if len(old_chain.shape) == 2:
-            c = old_chain.reshape((nwalkers, -1, num_params))
-            pos0 = c[:, -1, :]
-        else:  # 3
-            pos0 = old_chain[:, -1, :]
+        pos0 = _resume_from_previous(resume_from_previous, nwalkers, num_params)
     else:
-        pos0 = np.zeros((nwalkers, num_params))
-        for idx, pname in enumerate(param_names):
-            low, high = _emu.get_param_bounds(pname)
-            pos0[:, idx] = np.random.randn(nwalkers) * (np.abs(high - low) / 6.0) + (low + high) / 2.0
+        pos0 = _random_initial_guess(param_names, nwalkers, num_params)
 
     # TODO turn this into a generator
     sampler.run_mcmc(pos0, nsteps)
@@ -177,3 +226,72 @@ def run_mcmc(emu, cat, param_names, y, cov, r_bin_centers, obs_nd, obs_nd_err, n
     chain = sampler.chain[:, nburn:, :].reshape((-1, num_params))
 
     return chain
+
+def run_mcmc_iterator(emu, cat, param_names, y, cov, r_bin_centers, obs_nd, obs_nd_err, nd_func_name, \
+             fixed_params={}, resume_from_previous=None, nwalkers=1000, nsteps=100, nburn=20, ncores='all',\
+                      iterations = 100):
+    """
+    Run an MCMC using emcee and the emu. Includes some sanity checks and does some precomputation.
+    Also optimized to be more efficient than using emcee naively with the emulator.
+
+    As opposed to run_mcmc, this function also yields every 100 steps of the sampler, to write to file or print
+    incrementally. More safe if you're running a long chain that you're worried about timing out.
+
+    :param emu:
+        A trained instance of the Emu object
+    :param param_names:
+        Names of the parameters to constrain
+    :param y:
+        data to constrain against
+    :param cov:
+        measured covariance of y
+    :param r_bin_centers:
+        The scale bins corresponding to y
+    :param obs_nd
+        Observed number density
+    :param obs_nd_err
+        Uncertainty in the observed nd
+    :param nd_func
+        Function that can compute the number density given a dictionary of HOD params.
+    :param fixed_params:
+        Any values held fixed during the emulation, default is {}
+    :param nwalkers:
+        Number of walkers for the mcmc. default is 1000
+    :param nsteps:
+        Number of steps for the mcmc. Default is 1--
+    :param nburn:
+        Number of burn in steps, default is 20
+    :param ncores:
+        Number of cores. Default is 'all', which will use all cores available
+    :param: iterations:
+        Number of iterations to yield each step of the chain. Default is 100.
+    :yield:
+        chain, collaposed to the shape ((nsteps-nburn)*nwalkers, len(param_names))
+    """
+
+    _emu = emu
+    _cat = cat
+    global _emu
+    global _cat
+
+    ncores = _run_tests(y, cov, r_bin_centers, param_names, fixed_params, ncores, nd_func_name)
+    num_params = len(param_names)
+
+    combined_inv_cov = inv(np.diag(_emu.ycov) + cov)
+
+    sampler = mc.EnsembleSampler(nwalkers, num_params, lnprob,
+                                 threads=ncores, args=(param_names, fixed_params, r_bin_centers, y, combined_inv_cov, \
+                                                       obs_nd, obs_nd_err, nd_func_name))
+
+    if resume_from_previous is not None:
+        try:
+            assert nburn == 0
+        except AssertionError:
+            raise AssertionError("Cannot resume from previous chain with nburn != 0. Please change! ")
+        # load a previous chain
+        pos0 = _resume_from_previous(resume_from_previous, nwalkers, num_params)
+    else:
+        pos0 = _random_initial_guess(param_names, nwalkers, num_params)
+
+    for result in sampler.sample(pos0, iterations=iterations, storechain=False):
+        yield result[0]
