47b441f16a375d1fda9cbf74ead32dc10bca0c82
Sean McLaughlin <mclaughlin6464@gmail.com>
2017-10-06 11:41:52 -0700

    Made this branch to test if leaving off emluator errors biases the result.

--- bin/test_training.py
+++ bin/test_training.py
@@ -1,4 +1,7 @@
 #!/.conda/envs/hodemulator/bin/python
 from pearce.emulator import make_training_data
+from pearce.emulator import DEFAULT_PARAMS as ordered_params
+ordered_params['f_c'] = (0.95, 1.0) #I don't have a way to fixed this yet...
 
-make_training_data('/u/ki/swmclau2/Git/pearce/bin/training_config.cfg')
+make_training_data('/u/ki/swmclau2/Git/pearce/bin/training_config.cfg', \
+                    ordered_params = ordered_params)
