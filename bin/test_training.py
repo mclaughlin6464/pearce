275331070244da1ff435c530350b64ed3f4ce367
Sean McLaughlin <mclaughlin6464@gmail.com>
2017-10-18 10:53:19 -0700

    Tweaked training for sham again.

--- bin/test_training.py
+++ bin/test_training.py
@@ -4,6 +4,7 @@ from pearce.emulator import DEFAULT_PARAMS as ordered_params
 ordered_params['f_c'] = (0.95, 1.0) #I don't have a way to fixed this yet...
 ordered_params['logMmin'] = (12.5, 13.5)
 ordered_params['sigma_logM'] = (0.2, 1.0)
+ordered_params['logM0'] = (13.0, 16.0)
 
 make_training_data('/u/ki/swmclau2/Git/pearce/bin/training_config.cfg', \
                     ordered_params = ordered_params)
