860bff106944b685e8a27e211714ac6208b5c698
Sean McLaughlin <mclaughlin6464@gmail.com>
2019-12-09 09:22:54 -0800

    merging

--- bin/cache/cache_halocat.py
+++ bin/cache/cache_halocat.py
@@ -5,7 +5,7 @@ from pearce.mocks.kittens import cat_dict
 #0.85474
 #0.8112
 #cat = cat_dict['ds_14_b'](scale_factors = [0.538461538462], system = 'sherlock')
-#cat = cat_dict['chinchilla'](400.0, scale_factors = [1.0], system = 'sherlock')
+cat = cat_dict['chinchilla'](400.0, scale_factors = [1.0], system = 'ki-ls')
 #cat = cat_dict['ds_14_b'](scale_factors = [0.0], system = 'sherlock')
 #cat = cat_dict['multidark_highres'](scale_factors = [1.00110], system = 'sherlock')
 #cat.cache(overwrite = True, add_local_density=False)#, add_particles=True,downsample_factor = 1e-3)
@@ -14,8 +14,8 @@ from pearce.mocks.kittens import cat_dict
 #    for realization in xrange(1):
 #for boxno in xrange(19,40):
      #cat = cat_dict['testbox'](boxno, realization,scale_factors = [1.0], system = 'ki-ls')
-boxno = 0
-cat = cat_dict['trainingbox'](boxno, scale_factors = [1.0], system = 'ki-ls')
+#boxno = 0
+#cat = cat_dict['trainingbox'](boxno, scale_factors = [1.0], system = 'ki-ls')
     #cat = cat_dict['resolution'](boxno, scale_factors = [0.8, 1.0], system = 'ki-ls')
 
 cat.cache(overwrite = True, add_local_density=True, add_particles = True, downsample_factor = 1e-2)
--- pearce/emulator/trainer.py
+++ pearce/emulator/trainer.py
@@ -654,7 +654,7 @@ class Trainer(object):
             call(command, shell=self.system == 'sherlock')
 
 
-def make_kils_command(jobname, max_time, outputdir, queue= 'bulletmpi'):
+def make_kils_command(jobname, max_time, outputdir, queue= 'long'):
     '''
     Return a list of strings that comprise a bash command to call trainingHelper.py on the cluster.
     Designed to work on ki-ls's batch system
