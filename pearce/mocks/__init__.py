64673996ed7469b35a7ccb29f22f3507f3faeb01
Sean McLaughlin <mclaughlin6464@gmail.com>
2020-05-01 13:49:55 -0700

    merging

--- pearce/mocks/__init__.py
+++ pearce/mocks/__init__.py
@@ -4,3 +4,4 @@ from .kittens import *
 from .assembias_models import *
 from .tpcfSubregions import *
 from .void_density_function import void_density_function
+from readGadgetSnapshot import readGadgetSnapshot
